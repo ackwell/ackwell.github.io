ffb629727ba84807bb9431e0ebe8e7dc036ba8cc
Saxon Landers <saxon@canva.com>
2020-05-21 17:48:27 +1000

    Add syntax highlighting

--- main.js
+++ main.js
@@ -16,8 +16,25 @@ function onReady() {
 		updateTheme()
 	})
 
+	// Extract the language of code blocks and add as a data attribute for use from css
+	for (const langElem of document.querySelectorAll('[class*="language-"]')) {
+		let language
+		for (const className of langElem.classList) {
+			if (!className.startsWith('language-')) { return }
+			language = className.split('-')[1]
+			break
+		}
+
+		let codeElem = langElem
+		if (codeElem.tagName.toLowerCase() !== 'code') {
+			codeElem = langElem.querySelector('code')
+		}
+
+		codeElem.setAttribute('data-language', language)
+	}
+
 	// Set target=_blank on all external links
-	for (link of document.querySelectorAll('a')) {
+	for (const link of document.querySelectorAll('a')) {
 		if (link.hostname === window.location.hostname) {
 			continue
 		}
