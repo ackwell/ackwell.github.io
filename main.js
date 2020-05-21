48e438bebbdd005e348ca486f0b801e97cba6b7c
Saxon Landers <saxon@canva.com>
2020-05-21 15:24:06 +1000

    Add dark mode

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 function onReady() {
+	// Handle theme switching via light switch
+	let darkMode = localStorage.getItem('lightswitch') === 'dark'
+	const lightswitch = document.querySelector('.lightswitch')
+	const lightswitchIcon = lightswitch.querySelector('i')
+	function updateTheme() {
+		lightswitchIcon.classList.remove('fa-moon', 'fa-sun')
+		lightswitchIcon.classList.add(darkMode ? 'fa-moon' : 'fa-sun')
+
+		document.body.classList.toggle('dark', darkMode)
+	}
+	updateTheme()
+	lightswitch.addEventListener('click', function() {
+		darkMode = !darkMode
+		localStorage.setItem('lightswitch', darkMode ? 'dark' : 'light')
+		updateTheme()
+	})
+
 	// Set target=_blank on all external links
 	for (link of document.querySelectorAll('a')) {
 		if (link.hostname === window.location.hostname) {
